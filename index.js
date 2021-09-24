cd62a93e37dae6e8e379a8c7a13018ca6a509d62
neivm <neimv.zatara@gmail.com>
2021-09-24 14:57:29 -0500

    begins

--- index.js
+++ index.js
@@ -8,6 +8,9 @@
 const express = require('express');
 const app = express();
 
+// body parser
+app.use(express.json());
+
 const config = require('./config/index')
 const  moviesApi = require('./routes/movies.js')
 
